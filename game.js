caab97f1582d8dd246c37922fa55012219c4a309
sallybananas <saljgiani@gmail.com>
2017-12-12 18:37:14 -0700

    first push

---
+++ game.js
@@ -0,0 +1,106 @@
+var inquirer = require("inquirer");
+var figlet = require("figlet");
+var clear = require('clear');
+var private = require('private');
+
+// my modules
+var getQuote = require("./lib/quoteAPI.js");
+var printColor = require("./lib/printColor.js");
+var gameStats = require("./lib/gameStats.js");
+var Stats = new gameStats();
+
+// Start the game
+playGame(true);
+
+/******* Part I. ******
+* use inquirer to ask if user wants to play
+* YES! --> getQuote() --> and pass quoteObj to second part of inquirer
+* NO --> say goodbye.
+*/
+function playGame(firstTimeBool){
+  question = { message: "Play another game?", type: "confirm", name: "play"};
+  if (firstTimeBool){
+    clear();
+    console.log(figlet.textSync("Welcome!"));
+    question["message"] = "Want to play hangman?";networkInterfaces
+  }
+  // inquirer prompt!
+  inquirer.prompt(question)
+  .then(function(input){
+    if (input.play){
+      clear()
+      getQuote().then(quoteObj => {
+        guessLetter(false, quoteObj);
+      })
+      .catch(err => {console.log(err)});
+    } else {
+      clear();
+      Stats.showOptions();
+    }
+  }) // .then of inquirer
+};
+
+/******* Part II. ******
+* Keep asking the user to guess a letter, as long as they have not
+* already guessed the correct word.
+*/
+function guessLetter(againBool, quoteObj){
+  if (!againBool){
+    console.log(`Who said ... \n"${quoteObj.quote}"`);
+    quoteObj.word.show();
+  }
+
+  if (quoteObj.word.incorrectLetters.length !== 0){
+    console.log(`Incorrect Letter(s): ${quoteObj.word.incorrectLetters.join(", ")}`);
+  }
+  // build the question
+  var question = {
+    type: "input", 
+    name: "letter", 
+    validate: function(value){
+      var regexp = /^[a-zA-Z]{1}$/gi;
+      return regexp.test(value) ? true : "please enter only one letter";
+    }
+  };
+  question["message"] = againBool ? "Guess another letter: " :  "Guess a letter: ";
+
+  // prompt question!
+  inquirer.prompt(question)
+  .then(function(input){
+    clear(); 
+    var guess = input.letter;
+    console.log(`Who said ... \n"${quoteObj.quote}"`);
+    // if (process.env.DEBUG) console.log(quoteObj.word.author);
+
+    // 1. check if the letter is in word
+    var result = quoteObj.word.hasLetter(input.letter);
+    var message;
+    if (result.correct){
+      quoteObj.word.show();
+      message = result.newGuess ?
+      `Nice! we found a(n) "${guess.toUpperCase()}"`:
+      `You already guessed a(n) "${guess.toUpperCase()}" - silly!`;
+      console.log(printColor.success(message));
+    } else {
+      quoteObj.word.show();
+      message = result.newGuess ? 
+        `Sorry, no "${guess.toUpperCase()}" found` : 
+        `Sorry bud, "${guess.toUpperCase()}" still isn't the right letter.`;
+      console.log(printColor.wrong(message));
+    };
+
+    // 2. check if user has won
+    if (quoteObj.word.solved()){
+          console.log(figlet.textSync("You WON!!!"));
+          console.log(`You had ${quoteObj.word.incorrectGuess} incorrect guess(es)`);
+          Stats.addCorrect(quoteObj);
+          playGame(false);
+    } else if (quoteObj.word.incorrectGuess > 7){
+      console.log(`Out of Guesses! \n The answer was: ${quoteObj.word.showAll()}`);
+      Stats.addIncorrect(quoteObj);
+      playGame(false);
+    } else {
+      guessLetter(true, quoteObj);
+    }
+  }) // .then()
+}; // closes guessLetter()
